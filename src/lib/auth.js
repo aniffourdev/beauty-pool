0696f3c45087cb691009d0ce88ff8c77b9016443
Hamza Aniffour <aniffourmersh@gmail.com>
2024-12-12 11:29:47 +0100

    Google Auth Error Fixed

--- src/lib/auth.js
+++
@@ -1,8 +0,0 @@
-// utils/auth.js
-export const refreshToken = async () => {
-    await fetch('https://maoulaty.shop/auth/refresh', {
-      method: 'POST',
-      credentials: 'include',
-    });
-  };
-  
