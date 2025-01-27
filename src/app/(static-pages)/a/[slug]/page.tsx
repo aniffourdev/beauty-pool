4c37920d341283f5d6d24bc479f76c0ea37536ef
Hamza Aniffour <aniffourmersh@gmail.com>
2025-01-27 12:39:14 +0100

    Single Salon Page Fixed 5

--- src/app/(static-pages)/a/[slug]/page.tsx
+++ src/app/(static-pages)/a/[slug]/page.tsx
@@ -12,36 +12,20 @@
 // export default Page;
 
 
-import React from "react";
 import SingleBook from "@/components/dynamic/Book/SingleBook";
 import BookingHeader from "@/components/global/booking-header/BookingHeader";
-import { Metadata } from 'next';
 
-interface GenerateMetadataProps {
-  params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export async function generateMetadata(
-  { params }: GenerateMetadataProps,
-): Promise<Metadata> {
-  return {
-    title: `Article - ${params.slug}`,
-  };
-}
-
-export default async function ArticlePage({
+// Define page props without async
+export default function ArticlePage({
   params,
 }: {
   params: { slug: string };
 }) {
-  const { slug } = params;
-
   return (
     <>
       <BookingHeader />
       <div className="relative top-28">
-        <SingleBook slug={slug} />
+        <SingleBook slug={params.slug} />
       </div>
     </>
   );
